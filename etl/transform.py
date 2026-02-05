aafe00c7a34ea42482e769b883aa497cd23f4d78
Mutian He <austin10231@outlook.com>
2026-02-04 16:09:48 -0800

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -16,74 +16,34 @@ def build_user_features(prior_orders: pd.DataFrame) -> pd.DataFrame:
     user_order_hod = prior_orders.groupby("user_id")["order_hour_of_day"].mean().rename("avg_order_hour").reset_index()
     user_dow = prior_orders.groupby("user_id").agg(user_dow=("order_dow", lambda s: s.mode().iloc[0])).reset_index()
     
-    user_feat = (
-        user_order_cnt
-        .merge(user_gaps, on="user_id", how="left")
-        .merge(user_order_hod, on="user_id", how="left")
-        .merge(user_dow, on="user_id", how="left")
-    )
+    user_feat = user_order_cnt.merge(user_gaps, on="user_id", how="left").merge(user_order_hod, on="user_id", how="left").merge(user_dow, on="user_id", how="left")
     return user_feat
 
 def build_product_features(opp: pd.DataFrame, prior_orders: pd.DataFrame) -> pd.DataFrame:
     prior_order_ids = prior_orders['order_id']
     prior_opp = opp[opp['order_id'].isin(prior_order_ids)]
     
-    product_feat = (
-        prior_opp.groupby("product_id")
-        .agg(
-            product_cnt=("order_id", "count"),
-            product_reorder_rate=("reordered", "mean"), 
-        )
-        .reset_index()
-    )
+    product_feat = prior_opp.groupby("product_id").agg(
+        product_cnt=("order_id", "count"),
+        product_reorder_rate=("reordered", "mean")
+    ).reset_index()
     
     opp_user = prior_opp.merge(prior_orders[["order_id", "user_id"]], on="order_id", how="left")
     product_user = opp_user.groupby("product_id", as_index=False).agg(product_unique_user=("user_id", "nunique"))
     
-    product_feat = product_feat.merge(product_user, on="product_id", how="left")
-    return product_feat
+    return product_feat.merge(product_user, on="product_id", how="left")
 
-def build_user_product_features(opp: pd.DataFrame, prior_orders: pd.DataFrame, user_feat: pd.DataFrame) -> pd.DataFrame:
-    # 【修复泄露】同样只使用 prior 数据
-    prior_order_ids = prior_orders['order_id']
-    prior_opp = opp[opp['order_id'].isin(prior_order_ids)]
+def assemble_training_table(opt_new, user_feat, product_feat) -> pd.DataFrame:
+    df = opt_new.merge(user_feat, on="user_id", how="left").merge(product_feat, on="product_id", how="left")
     
-    opp_u = prior_opp.merge(
-        prior_orders[["order_id", "user_id", "order_number"]],
-        on="order_id",
-        how="left"
-    )
-    
-    up_cnt = opp_u.groupby(["user_id", "product_id"], as_index=False).agg(up_cnt=("order_id", "count"))
-    
-    up_last_order = opp_u.groupby(["user_id", "product_id"], as_index=False).agg(up_last_order=("order_number", "max"))
-    
-    up_feat = up_cnt.merge(user_feat[["user_id", "user_order_cnt"]], on="user_id", how="left")
-    up_feat["up_ratio"] = up_feat["up_cnt"] / up_feat["user_order_cnt"]
-    up_feat = up_feat.drop(columns=["user_order_cnt"])
-    
-    up_feat = up_feat.merge(up_last_order, on=["user_id", "product_id"], how="left")
-    return up_feat
-
-def assemble_training_table(opt_new, user_feat, product_feat, up_feat) -> pd.DataFrame:
-
-    df = (
-        opt_new
-        .merge(user_feat, on="user_id", how="left")
-        .merge(product_feat, on="product_id", how="left")
-        .merge(up_feat, on=["user_id", "product_id"], how="left")
-    )
-    
-    fill_cols = ["up_cnt", "up_last_order", "up_ratio", "product_cnt", "product_reorder_rate", "product_unique_user"]
+    fill_cols = ["product_cnt", "product_reorder_rate", "product_unique_user"]
     for c in fill_cols:
         if c in df.columns:
             df[c] = df[c].fillna(0)
-            
     return df
 
 def build_feature_lists():
     USER_FEATURE = ["user_order_cnt", "mean_days_since_last_order", "avg_order_hour", "user_dow"]
     PRODUCT_FEATURE = ["product_cnt", "product_unique_user", "product_reorder_rate"]
-    UP_FEATURE = ["up_cnt", "up_last_order", "up_ratio"]
-    FEATURE = USER_FEATURE + PRODUCT_FEATURE + UP_FEATURE
-    return USER_FEATURE, PRODUCT_FEATURE, UP_FEATURE, FEATURE
+    FEATURE = USER_FEATURE + PRODUCT_FEATURE
+    return USER_FEATURE, PRODUCT_FEATURE, [], FEATURE
